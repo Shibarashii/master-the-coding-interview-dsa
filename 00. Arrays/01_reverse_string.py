57ab698babbb490432ee110ddaac9e8c1b5d4921
Shiba <seanrei.ethan23@gmail.com>
2025-11-22 22:27:21 +0800

    add reverse string

---
+++ 00. Arrays/01_reverse_string.py
@@ -0,0 +1,10 @@
+def reverse_string(string: str) -> str:
+    reversed_string = ""
+    for i in range(len(string) - 1, -1, -1):
+        reversed_string += string[i]
+
+    return reversed_string
+
+
+a = reverse_string("hello")
+print(a)
