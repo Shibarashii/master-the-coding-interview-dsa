777736444cdc53fc3687dcee1e792ce22b48f5f4
Shiba <seanrei.ethan23@gmail.com>
2025-12-01 16:39:21 +0800

    implement graph

---
+++ 05. Graphs/00_graph.py
@@ -0,0 +1,36 @@
+class Graph:
+    def __init__(self):
+        self.size = 0
+        self.adjacent_list: dict[str, list[str]] = {}
+
+    def add_vertex(self, node):
+        self.adjacent_list[node] = []
+        self.size += 1
+
+    def add_edge(self, node1, node2):
+        self.adjacent_list[node2].append(node1)
+        self.adjacent_list[node1].append(node2)
+
+    def show_connections(self):
+        for key, item in self.adjacent_list.items():
+            print(f"{key} --> {" ".join(i for i in item)}")
+
+
+my_graph = Graph()
+my_graph.add_vertex('0')
+my_graph.add_vertex('1')
+my_graph.add_vertex('2')
+my_graph.add_vertex('3')
+my_graph.add_vertex('4')
+my_graph.add_vertex('5')
+my_graph.add_vertex('6')
+my_graph.add_edge('3', '1')
+my_graph.add_edge('3', '4')
+my_graph.add_edge('4', '2')
+my_graph.add_edge('4', '5')
+my_graph.add_edge('1', '2')
+my_graph.add_edge('1', '0')
+my_graph.add_edge('0', '2')
+my_graph.add_edge('6', '5')
+
+my_graph.show_connections()
